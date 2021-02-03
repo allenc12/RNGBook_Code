0513d7bfd46fae4dce1b44a415dfe4abbef1b258
Clayton Allen <mrchafeits@aaathats3as.com>
2021-02-03 15:17:37 -0800

    fix: ble.py
    original file was invalid python

--- ble.py
+++ ble.py
@@ -1,8 +1,8 @@
-# ???
+# 2-EXT Extractor Blender
 def ble(x3,x2,x1,x0,y3,y2,y1,y0):
     out0 = x3*y3 ^ x2*y2 ^ x1*y1 ^ x0*y0
-    out1 = X_2 Y_3 + X_1 Y_2 + X_3 X_0 Y_1 + X_3 Y_0
-    out2 = X_1 Y_3 + X_3 X_0 Y_2 + X_3 X_2 X_1 + X_2 Y_0
-    out3 = X_3 X_0 Y_3 + X_3 X_2 Y_2 + X_2 X_1 X_1 + X_1 Y_0
+    out1 = x2*y3 ^ x1*y2 ^ x3*x0*y1 ^ x3*y0
+    out2 = x1*y3 ^ x3*x0*y2 ^ x3*x2*x1 ^ x2*y0
+    out3 = x3*x0*y3 ^ x3*x2*y2 ^ x2*x1*x1 ^ x1*y0
     return (out3, out2, out1, out0)
 
